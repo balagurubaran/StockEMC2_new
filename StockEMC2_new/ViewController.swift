9717834a9a0de859b21f07e4c76a5ab9b2774b39
Balagurubaran Kalingarayan <pragadha@balaguraransmbp.home>
2018-11-11 20:49:29 -0500

    Initial Commit

---
+++ StockEMC2_new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockEMC2_new
+//
+//  Created by Balagurubaran Kalingarayan on 11/11/18.
+//  Copyright © 2018 iAppsCrazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
